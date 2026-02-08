b359f653289bccd953d8ce79adff3ef7b6a10d3c
naveelkazi-dev <naveelkazi@gmail.com>
2026-02-09 00:46:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder:Kazi Naveel
+# Date:30/1/26
 
 print("--- Factorial Finder ---\n")
 
+num = int(input("Enter Number: "))
 
-# Write your code here
+# Check for negative number
+if num < 0:
+    print(f"Factorial of {num} is Not Defined")
+else:
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+
+    print(f"Factorial of {num} is {factorial}")
+
+
